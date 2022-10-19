efa52be94fd10751eaee63eaa8b9ba6012f67347
paullaster <paullasterokoth98@gmail.com>
2022-10-19 08:58:23 +0300

    ..

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 //variables
-const titles = "This is a React App title"
+const welcome = {
+  greeting: "Hello, world!",
+  titles: "This is a React App title"
+}
 
 function App() {
   return (
     <div>
-      Hello World! {titles}
-      <label htmlFor= "try" > Try:</label>
-      <input type='text' id="try" placeholder="Try..." />
+      {welcome.greeting} {welcome.titles}
     </div>
   );
 }
