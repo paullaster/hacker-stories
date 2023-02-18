8f8200d8ff5471d410dee4b75c96b16b403d6189
paullaster <paullasterokoth98@gmail.com>
2023-02-18 22:09:57 +0300

    commit changes

--- src/App.js
+++ src/App.js
@@ -243,7 +243,7 @@ const App = () => {
       <h1>
         Welcome to {getTitle("Hacker Stories")}
       </h1>
-      <form>
+      <form onSubmit={handleSearchSubmit}>
         <InputWithLabel 
         id="search"
         label="search:"
