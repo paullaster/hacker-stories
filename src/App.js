bba9b94684450359a24136c220650fcd17e2cc5b
paullaster <paullasterokoth98@gmail.com>
2023-02-21 07:09:12 +0300

    test cases

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import axios from 'axios';
 import styles from "./App.module.css";
 import classNames from 'classnames';
 import { ReactComponent as Check } from './check.svg';
-
+import axios from 'axios';
 //variables
 export const getTitle = (title) => {
   return title;
