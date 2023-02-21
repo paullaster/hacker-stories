ffc1e5c1533cbe80e6c9d130a54b9faaebeb681c
paullaster <paullasterokoth98@gmail.com>
2023-02-21 06:47:52 +0300

    test cases

--- src/App.test.js
+++ src/App.test.js
@@ -10,25 +10,36 @@ import App, {
   getSumComments
 } from "./App";
 
-describe('something truthy and falsy', () => {
- test('true to be true', () => {
-  expect(true).toBeTruthy();
- });
-
- test('false to be false', () => {
-  expect(false).toBeFalsy();
- });
-});
-
-describe('App component', () => {
-  test('remove item when clicking remove item button', () => {
-
+describe("something truthy and falsy", () => {
+  test("true to be true", () => {
+    expect(true).toBeTruthy();
   });
-  test('request some initial stories from API', () => {
 
+  test("false to be false", () => {
+    expect(false).toBeFalsy();
   });
 });
 
-describe('storiesReducer', () => {
-  
-})
+describe("App component", () => {
+  test("remove item when clicking remove item button", () => {});
+  test("request some initial stories from API", () => {});
+});
+
+const storyOne = {
+  title: "React",
+  url: "https://reactjs.org/",
+  author: "Jordan Walke",
+  num_comments: 3,
+  points: 4,
+  objectID: 0
+};
+const storyTwo = {
+  title: "Redux",
+  url: "https://redux.js.org/",
+  author: "Dan Abramov, Andrew Clark",
+  num_comments: 2,
+  points: 5,
+  objectID: 1
+};
+
+describe("storiesReducer", () => {});
